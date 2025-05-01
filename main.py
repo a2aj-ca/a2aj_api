520e019b1ef277e858f12210e10dea87621a01c1
Simon Wallace <100489298+simon-lawyer@users.noreply.github.com>
2025-05-01 15:38:25 +0000

    establish connection between API and DB

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+import os
+from pymongo import MongoClient
+
+# Check if production or development
+if os.getenv("MONGO_DB") == None:
+    DB_URL = "mongodb.railway.internal"
+else:
+    DB_URL = os.getenv("MONGO_DB")
+
+# Connect to MongoDB
+client = MongoClient(DB_URL)
+db = client["test"]
+collection = db['decisions']
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    # return items in collection
+    items = collection.find()
+    items_list = []
+    for item in items:
+        items_list.append(item)
+    return items_list
+    
